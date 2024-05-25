1e70cac0cc101d0de53c6974047263c3fa0131f6
Michael Brown <michaelpbrown.007@gmail.com>
2024-05-24 18:50:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,17 @@ setup(
     version= __version__,
     author="Natalie Norstad, Michael Brown, Sanaah Arramreddy",
     author_email="nnorstad@ucsd.edu, mibrown@ucsd.edu, sarramre@ucsd.edu",
-    description="Bioinformatics tool that estimates genome size and the best k-mer length."
+    description="Bioinformatics tool that estimates genome size and the best k-mer length.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     entry_points={  
         "console_scripts": [
             "kmerdemon=kmerdemon.program:main",
         ],
     },
+    install_requires=[
+        "argparse",
+        "matplotlib"
+    ]
 )
