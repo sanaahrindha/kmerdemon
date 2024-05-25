8f96d9d6cd816da83825dd80b9ac063e004fb722
Michael Brown <michaelpbrown.007@gmail.com>
2024-05-24 18:43:39 -0700

    basic setup

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
-
+from setuptools import setup, find_packages
+from kmerdemon.__version__ import __version__
+setup(
+    name='kmerdemon',
+    version= __version__,
+    author="Natalie Norstad, Michael Brown, Sanaah Arramreddy",
+    author_email="nnorstad@ucsd.edu, mibrown@ucsd.edu, sarramre@ucsd.edu",
+    description="Bioinformatics tool that estimates genome size and the best k-mer length."
+    packages=find_packages(),
+    entry_points={  
+        "console_scripts": [
+            "kmerdemon=kmerdemon.program:main",
+        ],
+    },
+)
