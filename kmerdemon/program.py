f4c35af41d057fbe6c436ec745b7a36ba72a6c4d
nnorstad <nnorstad@ucsd.edu>
2024-06-04 15:37:30 -0700

    please please

--- kmerdemon/program.py
+++ kmerdemon/program.py
@@ -347,10 +347,10 @@ def main():
 
     for kmer_size in range(min_kmer_size, max_kmer_size, increment):
         kmer_frequencies_by_size[kmer_size] = abundance(sample,kmer_size)
+        create_histogram(kmer_frequencies_by_size[kmer_size], optimal_kmer_length)
     optimal_kmer_length, num_unique_kmers = predict_best_k(kmer_frequencies_by_size)
     #num_unique_kmers = len(kmer_frequencies_by_size[optimal_kmer_length])
     best_distribution = kmer_frequencies_by_size[optimal_kmer_length]
-    create_histogram(best_distribution, optimal_kmer_length)
 
     '''if increment != 1:
         kmer_frequencies_by_size_better = {}
